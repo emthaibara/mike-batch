f2ffa42628f9f2d813605e60ef5452da83c4fead
lemt <893295815@qq.com>
2025-08-04 14:30:13 +0800

    需求变动

--- src/common/enums/Status.py
+++ src/common/enums/Status.py
@@ -1,9 +1,9 @@
 from enum import Enum,auto
 
 class StatusEnum(Enum):
-    COMPLETED  = 1
-    ERROR = 2
-    IN_PROGRESS  = 3
-    NOT_STARTED  = 4
+    COMPLETED  = auto()
+    ERROR = auto()
+    IN_PROGRESS  = auto()
+    NOT_STARTED  = auto()
 
 
--- src/script/InvokeFemEngine.py
+++ src/script/InvokeFemEngine.py
@@ -1,25 +1,19 @@
-import os
 import platform
-import orjson
-from tqdm import tqdm
 from joblib import Parallel, delayed
-from src.Check import init_tasks
 from multiprocessing import Manager,  freeze_support
-from src.common import script_generated_path
-from src.common.enums import StatusEnum
 from src.Check import *
 
 def invoke():
     # 将tasks设置为多进程共享变量,因为存在多进程更新任务状态的操作
     manager = Manager()
-    tasks = manager.list() # TODO 还有待考量
+    tasks = manager.list() # TODO: 还有待考量
     cases = []
 
     # 检查 cases.json, tasks.json 是否存在,检查顺序不可变动
     raw_cases = check_cases()
     raw_tasks = check_tasks()
 
-    # TODO 任务状态的更新还有待考量,也行不需要tasks = manager.list(), 耗时太久了
+    # TODO: 任务状态的更新还有待考量,也行不需要tasks = manager.list(), 耗时太久了
     __load_data(raw_cases, raw_tasks, tasks, cases,manager)
 
     # 筛选未开始模拟的case
@@ -37,6 +31,7 @@ def invoke():
             for task_id in tqdm(pending_tasks, desc="发起水动力模型模拟任务")
         )
     finally:
+        # TODO: 状态更新方式待定
         data = {'tasks':[]}
         for t in tqdm(tasks,desc='序列化tasks list'):
             data['tasks'].append(dict(t))
@@ -45,14 +40,15 @@ def invoke():
     
     print("All tasks done | updated tasks.json")
 
-# TODO 临时代码
+# TODO: 临时代码
 def __load_data(raw_cases,raw_tasks,tasks,cases,manager):
     for t in tqdm(raw_tasks, desc='加载tasks list'):
         tasks.append(manager.dict(t))
     cases[:] = raw_cases[:]
 
-#TODO: 调用水动力模型模拟引擎,并更新任务状态, 根据不同的任务设置不同的 dfs0 + m21fm   (具体修改哪些内容还不太明确)
+# TODO: 调用水动力模型模拟引擎,并更新任务状态, 根据不同的任务设置不同的 dfs0 + m21fm   (具体修改哪些内容还不太明确)
 def __worker(task_id,tasks,cases):
+
     tasks[task_id]['status'] = 1
     case = cases[task_id]['path']
     if not os.path.exists(os.path.join(script_generated_path,'simulation','all_case',case)):
@@ -61,7 +57,7 @@ def __worker(task_id,tasks,cases):
 # test
 if __name__ == '__main__':
     # mike模型模拟引擎 依赖windows 平台
-    # TODO 后续启动脚本,入门位置先检查系统平台类型,非 windows 则抛异常
+    # TODO: 后续启动脚本,入门位置先检查系统平台类型,非 windows 则抛异常
     p = platform.system()
     if p == 'Windows':
         freeze_support()
